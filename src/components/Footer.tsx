b3b89581661e24fb2c0718685cbf23a7f234e039
aliciusschroeder <alicius.schroeder@gmail.com>
2024-03-19 20:04:00 +0100

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,9 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className="bg-gray-700 text-gray-100 p-4 border-t border-gray-600">
-            {/*<div className="flex flex-col items-center justify-center">
-                <label>Copyright 2023 by Alicius Schröder</label>
-    </div>*/}
             <div className="flex flex-col items-center justify-center">
                 <div>
-                    © 2023 Alicius Schröder |&nbsp;
+                    © 2023-2024 Alicius Schröder |&nbsp;
                     <a
                         href="https://github.com/aliciusschroeder/pillcurve"
                         target="_blank"
