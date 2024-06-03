fe0255afe74bad23ab9baf49a88a8af4804ad654
aliciusschroeder <alicius.schroeder@gmail.com>
2024-06-03 02:48:18 -0500

    Remove mouse hover effect

--- src/components/ChartContainer.tsx
+++ src/components/ChartContainer.tsx
@@ -8,7 +8,7 @@ interface ChartContainerProps {
 const ChartContainer: React.FC<ChartContainerProps> = ({ children }) => {
   return (
     <section className="p-6 flex items-center justify-center flex-grow">
-      <div className="bg-gray-800 rounded-3xl shadow-2xl transform transition-all scale-95 hover:scale-100 flex-grow pr-8 py-8">
+      <div className="bg-gray-800 rounded-3xl shadow-2xl flex-grow pr-8 py-8">
         {children}
       </div>
     </section>
